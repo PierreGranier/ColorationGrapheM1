f87f2c76dbfec099549b0b3d087e862a190c8dbd
PierreGranier <piergranier@laposte.net>
2016-11-25 17:06:18 +0100

    correction ommettre -> omettre

--- structures/aretes_matrice.c
+++ structures/aretes_matrice.c
@@ -93,17 +93,17 @@ int clique_maximum() {
 	int max_clique = 0;
 	// Recherche de toutes les cliques en partant de chaque sommet du graphe
 	for (int i = 0; i < NOMBRE_DE_SOMMETS; ++i) {
-		// Recherche d'un sommet appartenant à la clique en les parcourant tous et en les ajoutant au tableau de clique s'ils sont aptes,à en faire partie
-		// Étant donné que l'ajout d'un sommet à la clique a une influence sur l'ajout ou pas des sommets suivant, il faut en ommettre certains pour qu'ils ne bloquent pas la situation
+		// Recherche d'un sommet appartenant à la clique en les parcourant tous et en les ajoutant au tableau de clique s'ils sont aptes à en faire partie
+		// Étant donné que l'ajout d'un sommet à la clique a une influence sur l'ajout ou pas des sommets suivant, il faut en omettre certains pour qu'ils ne bloquent pas la situation
 		// Donc boucle for supplémentaire pour trouver toutes les cliques possibles
-		// Ca sert à rien d'ommettre les x premiers sommets s'il ne reste que y sommets à regarder, y étant inférieur à la max clique trouvée, car il sera imppossible avec y sommets de faire une clique plus grande que celle déjà enregistrée
-		// for (int ommettre = 0; ommettre < NOMBRE_DE_SOMMETS; ++ommettre) {
-		for (int ommettre = 0; ommettre < (NOMBRE_DE_SOMMETS-max_clique); ++ommettre) {
+		// Ca sert à rien d'omettre les x premiers sommets s'il ne reste que y sommets à regarder, y étant inférieur à la max clique trouvée, car il sera imppossible avec y sommets de faire une clique plus grande que celle déjà enregistrée
+		// for (int omettre = 0; omettre < NOMBRE_DE_SOMMETS; ++omettre) {
+		for (int omettre = 0; omettre < (NOMBRE_DE_SOMMETS-max_clique); ++omettre) {
 			// printf("\nRecherche d'une clique avec le sommet %d :\n", i);
 			int voisins_de_clique[NOMBRE_DE_SOMMETS]; // tableau de sommets tous reliés entre eux
 			int nb_voisins_de_clique = 0;
 			voisins_de_clique[nb_voisins_de_clique++] = i;
-			for (int j = ommettre; j < NOMBRE_DE_SOMMETS; ++j) {
+			for (int j = omettre; j < NOMBRE_DE_SOMMETS; ++j) {
 				// printf("%d en lien avec : ", j);
 				// s'il y a une arête entre j et tous les sommets du tableau, ce sommet fait partie de la clique
 				int ajouter = 1;
